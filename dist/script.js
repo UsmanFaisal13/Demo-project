58595a149b774d05663b9372aa1e6784dd04f1e1
UsmanFaisal13 <130107471+UsmanFaisal13@users.noreply.github.com>
2025-04-01 05:20:55 +0500

    yes

--- dist/script.js
+++ dist/script.js
@@ -8,10 +8,4 @@ btn.addEventListener("click", () => {
   hamburgerIcon.classList.toggle("hidden"); // Toggle hamburger icon
   closeIcon.classList.toggle("hidden"); // Toggle close icon
 });
-const links = document.querySelectorAll("a");
 
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    alert("Since this is just a demo project, no other pages have been added");
-  });
-});
